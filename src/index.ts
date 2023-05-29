2867b7641c8c3d3fa4725ad67863d0623d29fb5f
Jackson Almeida <kinho160@homail.com>
2023-05-29 14:50:13 -0300

    updated

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/*
+Advantages and disadvantages of S.O.L.I.D
+
+Advantages:
+- modular code
+- reusable code (D.R.Y Don't depeat yourself)
+- test code, low coupling
+- low coupling and high cohesion
+- expansive code
+- separations of concerns
+- easy maintenace
+
+Disadvantages
+- complexity
+- amount of code typed increases
+- development time increases
+- be careful with: YAGN (You aren't gonna neet it) and the KISS (Keep it siimple, stupid!)
+*/
